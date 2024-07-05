a9167fc2ddc46c710144c0538c0a687da7bbd749
SOURAV SREELAN <98900624+SouravSreelan@users.noreply.github.com>
2024-07-05 16:36:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-
-
+   
+  
 function App() {
   const [toDos, setToDos] = useState([])
   const [toDo, setToDo] = useState('')
@@ -72,3 +72,4 @@ function App() {
 }
 
 export default App;
+  
